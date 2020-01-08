2dd18b7bb166c10997a0fbaedac827de99c61f21
Sean Lang <slang800@gmail.com>
2020-01-07 20:59:02 -0600

    fix amqp config in test env

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :database_worker, :lmdb, database_path: './test-db'
 
-config :database_worker, DatabaseWorker.RPCServer,
+config :database_worker, :amqp,
   host: "localhost",
   username: "301remover",
   password: "your_password_here",
