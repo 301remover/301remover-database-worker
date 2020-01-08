f7908a2554a533bebb56e4fecf23115290297238
Brandon Kandarapally <kandarapallybl@msoe.edu>
2020-01-08 14:27:55 -0600

    Succesfully sending unknown links to resolver worker and then adding resolved links to database for future use
    .

--- lib/database_worker/amqp_connection.ex
+++ lib/database_worker/amqp_connection.ex
@@ -5,6 +5,7 @@ defmodule DatabaseWorker.AmqpConnection do
     unless "#{Mix.env()}" === "test" do
       {:ok, conn} = Freddy.Connection.start_link(params)
       {:ok, _server} = DatabaseWorker.RPCServer.start_link(conn)
+      {:ok, _client} = DatabaseWorker.RPCClient.start_link(conn)
     else
       {:ok, conn} = Freddy.Connection.start_link(adapter: :sandbox)
       {:ok, _server} = MockServer.start_link(conn)
