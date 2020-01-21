481f2f667862969fd3ce1e1231448e6d8a55b6b6
Robert Laughlin <laughlinra@msoe.edu>
2020-01-21 03:13:58 +0000

    Fixed VirtualHost being set to "", causing failed connection to rabbitMQ.

--- config/dev.exs
+++ config/dev.exs
@@ -5,5 +5,4 @@ config :database_worker, :lmdb, database_path: "./301remover-db"
 config :database_worker, :amqp,
   host: "localhost",
   username: "301remover",
-  password: "your_password_here",
-  virtual_host: ""
+  password: "your_password_here"
