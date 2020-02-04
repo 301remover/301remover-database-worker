f3ceade8ddbf80109edfd95741103b089db98307
Sean Lang <slang800@gmail.com>
2020-02-04 15:11:45 -0600

    add todo to domain list in application.ex

--- lib/database_worker/application.ex
+++ lib/database_worker/application.ex
@@ -10,6 +10,7 @@ defmodule DatabaseWorker.Application do
 
     database_path = Application.get_env(:database_worker, :lmdb)[:database_path]
 
+    # TODO: these domain names should be fetched from the server
     domains = ["bit.ly", "goo.gl", "tinyurl.com"]
 
     children = [
